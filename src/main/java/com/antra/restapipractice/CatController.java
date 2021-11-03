177d9bfa2353c4fb46b93b08dd3eff184e559eb7
Dawei <dawei.zhuang@antra.com>
2021-11-03 12:12:28 -0400

    Added: fake data/return

--- src/main/java/com/antra/restapipractice/CatController.java
+++ src/main/java/com/antra/restapipractice/CatController.java
@@ -1,5 +1,6 @@
 package com.antra.restapipractice;
 
+import com.antra.restapipractice.pojo.CatFact;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CatController {
 
     @GetMapping("/catFact")
-    public ResponseEntity<?> getRandomFact() {
-        return null;
+    public ResponseEntity<?> getRandomFact() {  // TODO get random fact from "https://catfact.ninja/fact"
+        CatFact ret = new CatFact();
+        ret.setCatFact("this is a dummy fact");
+        return ResponseEntity.ok(ret);
     }
 }
---
+++ src/main/java/com/antra/restapipractice/pojo/CatFact.java
@@ -0,0 +1,13 @@
+package com.antra.restapipractice.pojo;
+
+public class CatFact {
+    private String catFact;
+
+    public String getCatFact() {
+        return catFact;
+    }
+
+    public void setCatFact(String catFact) {
+        this.catFact = catFact;
+    }
+}
